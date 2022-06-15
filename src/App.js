8941055cb18156d9638ab5a96190697ecfb76e42
pianistoff <severyn.h@gmail.com>
2022-06-15 21:10:32 +0300

    Finish the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Header from "./components/Header"
-import Card from "./components/Card"
+import Header from "./components/Header";
+import Card from "./components/Card";
+import data from "./data";
 
 export default function App() {
+    const cards = data.map((card) => <Card key={card.id} {...card} />);
     return (
         <div className="app">
             <Header />
-            <Card />
+            <div className="card-list">{cards}</div>
         </div>
-    )
+    );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,19 @@
-export default function Card() {
+import locationImage from "../img/location.png";
+
+export default function Card({title, location, googleMapsUrl, startDate, endDate, description, imageUrl}) {
     return (
-        <p>Hello</p>
+        <div className="card">
+            <img src={imageUrl} alt="An image of the destination place"  className="main-image" />
+            <div className="right-side">
+                <div className="first-line">
+                    <img src={locationImage} alt="" className="location-image" />
+                    <p className="location">{location.toUpperCase()}</p>
+                    <p className="view-on-map"><a href={googleMapsUrl} className="view-on-map-link">View on Google Maps</a></p>
+                </div>
+                <h2 className="title">{title}</h2>
+                <p className="date">{startDate} - {endDate}</p>
+                <p className="description">{description}</p>
+            </div>
+        </div>
     )
 }
